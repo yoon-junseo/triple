8bafe8faa14ebc129a5a5b3af3157d830078e9e9
jun5e00 <ysj6844@naver.com>
2022-07-04 20:02:39 +0900

    chore: lint, prettier 설정
    1. https://github.com/titicacadev/eslint-config-triple 참고하여 기존 설정 수정
    #1

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,1 @@
-module.exports = {
-  "printWidth": 120,
-  "tabWidth": 2,
-  "singleQuote": true,
-  "trailingComma": "all",
-  "semi": true,
-  "useTabs": false,
-  "arrowParens": "always",
-  "endOfLine": "auto",
-  "bracketSpacing": true,
-  "jsxBracketSameLine": true
-}
+'@titicaca/eslint-config-triple/prettierrc';
